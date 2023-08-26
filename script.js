8b3cf328cf17639510ea5b01aecae37a2271fba7
aa <81450053+negi-wasabi@users.noreply.github.com>
2023-08-26 09:36:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-// Placeholder for dynamic tweet loading
-const tweetsSection = document.querySelector('.tweets');
+// JavaScript functionality
+// Fetch tweets and user data from a backend API
+// Add event listeners for interactions (e.g., like, retweet, reply)
 
-// Placeholder for posting a new tweet
-const postButton = document.querySelector('.tweet-form button');
-postButton.addEventListener('click', () => {
-  const tweetText = document.querySelector('.tweet-form textarea').value;
-  if (tweetText.trim() !== '') {
-    // You can add the logic to create a new tweet here
-    const tweetItem = document.createElement('div');
-    tweetItem.className = 'tweet';
-    tweetItem.textContent = tweetText;
-    tweetsSection.appendChild(tweetItem);
-  }
-});
+// Example code
+const tweetsSection = document.getElementById('tweets');
+const userProfileDiv = document.getElementById('user-profile');
+const trendingTopicsDiv = document.getElementById('trending-topics');
+
+// Example function to fetch and display tweets
+function fetchAndDisplayTweets() {
+    // Fetch tweets from the backend API
+    const tweets = fetchTweetsFromAPI();
+
+    // Iterate through tweets and create tweet components
+    tweets.forEach(tweet => {
+        const tweetDiv = document.createElement('div');
+        tweetDiv.classList.add('tweet');
+        tweetDiv.innerHTML = `
+            <img src="${tweet.userProfileImage}" alt="User Profile">
+            <div class="tweet-content">
+                <p class="user-name">${tweet.userName}</p>
+                <p class="tweet-text">${tweet.text}</p>
+                <div class="tweet-actions">
+                    <button class="like-button">Like</button>
+                    <button class="retweet-button">Retweet</button>
+                    <button class="reply-button">Reply</button>
+                </div>
+            </div>
+        `;
+        tweetsSection.appendChild(tweetDiv);
+    });
+}
+
+// Fetch and display user profile
+function fetchAndDisplayUserProfile() {
+    // Fetch user data from the backend API
+    const user = fetchUserDataFromAPI();
+
+    // Display user profile information
+    userProfileDiv.innerHTML = `
+        <img src="${user.profileImage}" alt="User Profile">
+        <p>${user.userName}</p>
+        <p>${user.bio}</p>
+        <p>Followers: ${user.followers}</p>
+        <p>Following: ${user.following}</p>
+    `;
+}
+
+// Fetch and display trending topics
+function fetchAndDisplayTrendingTopics() {
+    // Fetch trending topics from the backend API
+    const trendingTopics = fetchTrendingTopicsFromAPI();
+
+    // Display trending topics
+    trendingTopicsDiv.innerHTML = `
+        <h2>Trending Topics</h2>
+        <ul>
+            ${trendingTopics.map(topic => `<li>${topic}</li>`).join('')}
+        </ul>
+    `;
+}
+
+// Fetch and display data when the page loads
+fetchAndDisplayTweets();
+fetchAndDisplayUserProfile();
+fetchAndDisplayTrendingTopics();
