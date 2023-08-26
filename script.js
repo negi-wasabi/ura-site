7f6504d8e4498e23cc6d43c2edc9ed285538cb83
aa <81450053+negi-wasabi@users.noreply.github.com>
2023-08-26 09:25:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const postForm = document.getElementById("post-form");
-    const authorInput = document.getElementById("author");
-    const messageInput = document.getElementById("message");
-    const postsList = document.getElementById("posts-list");
+// Placeholder for dynamic tweet loading
+const tweetsSection = document.querySelector('.tweets');
 
-    postForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        const author = authorInput.value;
-        const message = messageInput.value;
-
-        if (author.trim() === "" || message.trim() === "") {
-            alert("名前とメッセージを入力してください");
-            return;
-        }
-
-        // 新しい投稿を作成
-        const postItem = document.createElement("li");
-        postItem.innerHTML = `<strong>${author}</strong>: ${message}`;
-        postsList.appendChild(postItem);
-
-        // フォームをクリア
-        authorInput.value = "";
-        messageInput.value = "";
-    });
+// Placeholder for posting a new tweet
+const postButton = document.querySelector('.tweet-form button');
+postButton.addEventListener('click', () => {
+  const tweetText = document.querySelector('.tweet-form textarea').value;
+  if (tweetText.trim() !== '') {
+    // You can add the logic to create a new tweet here
+    const tweetItem = document.createElement('div');
+    tweetItem.className = 'tweet';
+    tweetItem.textContent = tweetText;
+    tweetsSection.appendChild(tweetItem);
+  }
 });
