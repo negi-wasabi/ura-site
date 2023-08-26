39f1f885c7339f240df97546fc42be37d76495f6
aa <81450053+negi-wasabi@users.noreply.github.com>
2023-08-26 10:01:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,45 @@ function fetchAndDisplayTrendingTopics() {
     `;
 }
 
+// 既存のJavaScriptコードをそのまま残します
+
+// ツイートの投稿フォームに対するイベントリスナーを追加
+const tweetForm = document.getElementById('tweet-form');
+const tweetTextArea = document.getElementById('tweet-text');
+
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // デフォルトのフォーム送信を防止
+
+    const tweetText = tweetTextArea.value; // ツイートのテキストを取得
+
+    // サーバーにツイートを投稿するためのHTTPリクエストを作成
+    // この部分はバックエンドの実装に依存します
+
+    // 例: サーバーにPOSTリクエストを送信
+    fetch('/api/tweets', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: tweetText }), // ツイートのテキストをJSON形式で送信
+    })
+    .then(response => response.json())
+    .then(data => {
+        // サーバーからの応答を処理する
+        console.log('Tweet posted:', data);
+        // ツイートが成功した場合、画面に表示したり、リストに追加するなどの操作を実行
+    })
+    .catch(error => {
+        console.error('Error posting tweet:', error);
+        // エラーが発生した場合、エラーメッセージを表示するなどの操作を実行
+    });
+    
+    // ツイートのテキストエリアをクリア
+    tweetTextArea.value = '';
+});
+
+
+
 // Fetch and display data when the page loads
 fetchAndDisplayTweets();
 fetchAndDisplayUserProfile();
